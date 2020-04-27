7123d31450b899dfea9a9cb90a6236b8ee29fdda
andhikaputrab <andhikaputra1301@gmail.com>
2020-04-27 12:50:52 +0700

    Shared Preference

---
+++ app/src/main/java/com/apps/andhika/latihansharedpreference/HomeActivity.java
@@ -0,0 +1,43 @@
+package com.apps.andhika.latihansharedpreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.apps.andhika.R;
+import com.apps.andhika.utils.Preferences;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView txtKeluar;
+    private TextView txtNama;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        declareView();
+        txtKeluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Clear set preferences
+                Preferences.setLogout(getBaseContext());
+
+                //Pindah halaman ke login
+                startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void declareView(){
+        txtKeluar = findViewById(R.id.txt_logout);
+        txtNama = findViewById(R.id.txtName);
+
+        txtNama.setText(Preferences.getRegisteredUsername(getBaseContext()));
+    }
+}
---
+++ app/src/main/java/com/apps/andhika/latihansharedpreference/LoginActivity.java
@@ -0,0 +1,119 @@
+package com.apps.andhika.latihansharedpreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.apps.andhika.R;
+import com.apps.andhika.com.apps.andhika.latihansharedpreference.model.UserModel;
+import com.apps.andhika.utils.Preferences;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView txtMasuk;
+    private TextView txtRegister;
+    private EditText edtUsername;
+    private EditText edtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        declareView();
+
+        txtMasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validasiLogin();
+            }
+        });
+
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), RegisterActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (Preferences.getLoggedInStatus(getBaseContext())) {
+            startActivity(new Intent(getBaseContext(), HomeActivity.class));
+            finish();
+        }
+    }
+
+    private void declareView(){
+        txtMasuk = findViewById(R.id.txt_login_masuk);
+        txtRegister = findViewById(R.id.txt_login_register);
+        edtUsername = findViewById(R.id.edt_login_username);
+        edtPassword = findViewById(R.id.edt_login_password);
+    }
+
+    private void validasiLogin(){
+        //reset semua error dan fokus menjadi default
+        edtUsername.setError(null);
+        edtPassword.setError(null);
+        View fokus = null;
+        boolean cancel = false;
+
+        //set input value dari view
+        String username = edtUsername.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        /* Jika form user kosong atau memenuhi kriteria di Method cekUser() maka, Set error di Form-
+        User dengan menset variable fokus dan error di Viewnya juga cancel menjadi true*/
+        if (TextUtils.isEmpty(username)){
+            edtUsername.setError("Username Belum diisi");
+            fokus = edtUsername;
+            cancel = true;
+        }else if (!cekUser(username)){
+            edtUsername.setError("Username Tidak Ditemukan");
+        }
+
+        if (TextUtils.isEmpty(password)){
+            edtPassword.setError("Password Belum Diisi");
+            fokus = edtPassword;
+            cancel = true;
+        }else if (!cekPassword(password)){
+            edtPassword.setError("Password Tidak sesuai");
+            fokus = edtPassword;
+            cancel = true;
+        }
+
+        /* Jika cancel true, maka variabel fokus mendapatkan fokus. Jika false, maka kembali
+           ke LoginActivity dan set User dan Password untuk data yang terdaftar */
+        if (cancel){
+            fokus.requestFocus();
+        }else{
+            //Deklarasi Model
+            UserModel userModel = new UserModel();
+            userModel.setUsername(username);
+            userModel.setPassword(password);
+
+            //Simpan data ke shared preferences
+            Preferences.setUserPreferences(getBaseContext(), userModel);
+            Preferences.setLoggedInStatus(getBaseContext(), true);
+
+            //Pindah halaman ke home
+            startActivity(new Intent(getBaseContext(), HomeActivity.class));
+            finish();
+        }
+    }
+
+    private boolean cekUser(String user){
+        return user.equals(Preferences.getRegisteredUsername(getBaseContext()));
+    }
+
+    private boolean cekPassword(String password){
+        return password.equals(Preferences.getRegisteredPassword(getBaseContext()));
+    }
+}
