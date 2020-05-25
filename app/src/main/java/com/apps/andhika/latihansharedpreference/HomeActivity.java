a423302d6d7450d5fe1d915f76751d4ae32a485c
andhikaputrab <andhikaputra1301@gmail.com>
2020-05-25 19:28:18 +0700

    Test Update

--- app/src/main/java/com/apps/andhika/latihansharedpreference/HomeActivity.java
+++ app/src/main/java/com/apps/andhika/latihansharedpreference/HomeActivity.java
@@ -15,6 +15,7 @@ import com.apps.andhika.utils.Preferences;
     on 27-04-2020
  */
 
+// TEST UPDATE PROJECT
 public class HomeActivity extends AppCompatActivity {
 
     private TextView txtKeluar;
